1eeb67671281f8c6a98fdb892c8b75db6b0af226
Sehee Hong <seaheehong@gmail.com>
2025-10-28 13:46:02 +0900

    project setting

---
+++ src/test/java/dev/hong/kurumi/KurumiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hong.kurumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
