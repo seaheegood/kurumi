4a6dde2c910d6fcdcde49a12444265297968f73b
Sehee Hong <seaheehong@gmail.com>
2025-10-29 15:56:58 +0900

    Add `MenuRepository` interface with JPA support

---
+++ src/main/java/dev/hong/kurumi/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package dev.hong.kurumi.repository;
+
+import dev.hong.kurumi.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
