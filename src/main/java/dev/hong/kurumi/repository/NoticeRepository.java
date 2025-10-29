8bc9951bf48802183239516d525eb2b2e379df50
Sehee Hong <seaheehong@gmail.com>
2025-10-29 16:01:20 +0900

    Add `NoticeRepository` interface with JPA support

---
+++ src/main/java/dev/hong/kurumi/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package dev.hong.kurumi.repository;
+
+import dev.hong.kurumi.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
